58047c9ef29fa67cd041af3421071796329b4d24
Roushan Kumar <rk04393@gmail.com>
2025-03-08 19:50:53 +0530

    update angular component to subscribe to data from angular service

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
+  products: Product[] = [];
 
+  //Inject the ProductService dependency
+  constructor(private productService: ProductService) {}
+
+  //this is similar to @PostConstruct in java
+  nqOnInit(): void {
+    this.listProducts();
+  }
+
+  //call the service to get the list of products this method is invoked once you subscribe.
+  listProducts() {
+    this.productService.getProductList().subscribe((data) => {
+      this.products = data; //assign the data to the products array
+    });
+  }
 }
