3f8cd0cdb5b2923d407829e2b7eb8152062a6671
Roushan Kumar <rk04393@gmail.com>
2025-03-08 19:59:51 +0530

    add cross orgin annotation

--- 02-backend/spring-boot-ecommerce/src/main/java/com/rOushAn/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/rOushAn/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,9 @@ package com.rOushAn.ecommerce.dao;
 
 import com.rOushAn.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+//Accept calls from web browser scripts for this orgin.
+@CrossOrigin("http//localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
